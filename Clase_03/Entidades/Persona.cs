daf7df31551d0145fd94fd6153ece9f56b8f0dd2
AriNunez <arielnunezpr@gmail.com>
2022-04-02 13:39:57 -0300

    Clase3 2-4

---
+++ Clase_03/Entidades/Persona.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objetos
+{
+    public class Persona
+    {
+        private string nombre;
+        private DateTime fechaDeNacimiento;
+        private string dni;
+
+        public void SetNombre(string nombre)
+        {
+            this.nombre = nombre;
+        }
+        public void SetFechaDeNacimiento(DateTime fechaDeNacimiento)
+        {
+            this.fechaDeNacimiento = fechaDeNacimiento;
+        }
+        public void SetDni(string dni)
+        {
+            if(int.TryParse)
+            this.dni = dni;
+        }
+
+        public string GetNombre()
+        {
+            return this.nombre;
+        }
+
+        public DateTime GetFechaDeNacimiento()
+        {
+            return this.fechaDeNacimiento;
+        }
+
+        public string GetDni()
+        {
+            return this.dni
+        }
+    }
+}
